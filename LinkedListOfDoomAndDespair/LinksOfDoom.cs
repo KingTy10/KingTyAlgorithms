7468d6abe5b63083bb0b063bc9dbeb10035d8356
KingTy10 <145054629+KingTy10@users.noreply.github.com>
2025-10-24 21:45:05 -0500

    Add files via upload

---
+++ LinkedListOfDoomAndDespair/LinksOfDoom.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+namespace LinkedListOfDoomAndDespair
+{
+    public class LinksOfDoom
+    {
+        public class LinkList<T>
+        {
+            private Node<T>? Head { get; set; }
+            private Node<T>? Tail { get; set; }
+            public int Count { get; private set; }
+
+            private class Node<T>
+            {
+                public T Value { get; set; }
+                public Node<T>? Next { get; set; }
+            }
+
+            // Add to the end of the list
+            public void Append(T data)
+            {
+                var newNode = new Node<T> { Value = data, Next = null };
+
+                if (Head == null)
+                {
+                    Head = Tail = newNode;
+                }
+                else
+                {
+                    Tail!.Next = newNode;
+                    Tail = newNode;
+                }
+
+                Count++;
+            }
+
+            // Add to the front of the list
+            public void Prepend(T data)
+            {
+                var newNode = new Node<T> { Value = data, Next = Head };
+                Head = newNode;
+
+                if (Tail == null)
+                    Tail = Head;
+
+                Count++;
+            }
+
+            // Get an element by position (0-indexed)
+            public T GetAt(int pos)
+            {
+                Node<T> temp = Head;
+                int i = 0;
+
+                while (i < pos && temp != null)
+                {
+                    temp = temp.Next;
+                    i++;
+                }
+
+                return temp != null ? temp.Value : default!;
+            }
+
+            // Remove first element and return it (used by Stack/Queue)
+            public T RemoveFirst()
+            {
+                if (Head == null) throw new InvalidOperationException("List is empty.");
+
+                T value = Head.Value;
+                Head = Head.Next;
+                if (Head == null) Tail = null;
+                Count--;
+                return value;
+            }
+
+            public bool IsEmpty() => Count == 0;
+
+            // For debugging
+            public void PrintAll()
+            {
+                Node<T> temp = Head;
+                while (temp != null)
+                {
+                    Console.Write(temp.Value + " -> ");
+                    temp = temp.Next;
+                }
+                Console.WriteLine("null");
+            }
+        }
+    }
+}
+
---
+++ LinkedListOfDoomAndDespair/QueueOfDOOM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LinkedListOfDoomAndDespair.LinksOfDoom;
+
+namespace LinkedListOfDoomAndDespair
+{
+    public class QueueOfDOOM<T> : LinksOfDoom.LinkList<T>
+    {
+        private Node<T> data;
+       private Node<T> Head;
+        public void Push(T data) 
+        {
+            Node<T> temp = this.Head;
+            Node<T> newNode = new Node<T>
+            {
+                Value = data,
+                Next = null
+
+
+            };
+            while (temp?.Next != null) 
+            {
+                temp= temp.Next;
+            
+            
+            }
+            if (temp != null)
+            { 
+                temp.Next = newNode;
+            
+            } 
+
+        }
+        //This si a pop but for the queue this is name dequeue
+        public void Dequeue() 
+        {
+
+            if (this.Head !=null) 
+            {
+                this.Head= this.Head.Next;
+            
+            }
+        }
+        public T Peek() 
+        {
+            if (data == null) 
+            {
+                return default(T);
+            
+            }
+            return Head.Value;
+        }
+        public bool IsEmpty()
+        {
+            return this.Head == null;   
+        }
+
+    }
+}
