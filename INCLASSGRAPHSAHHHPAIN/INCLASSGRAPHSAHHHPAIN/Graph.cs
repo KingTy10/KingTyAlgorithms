9cb31b14217b474fdb41e427ea3269509c3ad785
KingTy10 <145054629+KingTy10@users.noreply.github.com>
2025-12-10 01:55:44 -0600

    Add files via upload

---
+++ INCLASSGRAPHSAHHHPAIN/INCLASSGRAPHSAHHHPAIN/Graph.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INCLASSGRAPHSAHHHPAIN
+{
+    internal class Graph
+    {
+        private readonly int _V;              
+        private int _E;                       
+        private bool[,] adjMatrix;            
+
+        public Graph(int V)
+        {
+            _V = V;
+            _E = 0;
+            adjMatrix = new bool[V, V];
+        }
+
+     
+        public int V()
+        {
+            return _V;
+        }
+
+    
+        public int E()
+        {
+            return _E;
+        }
+
+  
+        public void AddEdge(int v, int w)
+        {
+            if (!adjMatrix[v, w])
+            {
+                adjMatrix[v, w] = true;
+                adjMatrix[w, v] = true;
+                _E++;
+            }
+        }
+
+      
+        public IEnumerable<int> Adj(int v)
+        {
+            for (int i = 0; i < _V; i++)
+            {
+                if (adjMatrix[v, i])
+                    yield return i;
+            }
+        }
+
+     
+        public int Degree(int v)
+        {
+            int count = 0;
+            for (int i = 0; i < _V; i++)
+            {
+                if (adjMatrix[v, i])
+                    count++;
+            }
+            return count;
+        }
+
+    
+        public int MaxDegree()
+        {
+            int max = 0;
+            for (int v = 0; v < _V; v++)
+            {
+                int d = Degree(v);
+                if (d > max)
+                    max = d;
+            }
+            return max;
+        }
+
+      
+        public float AverageDegree()
+        {
+            return (float)(2.0 * _E) / _V;
+        }
+
+ 
+        public int NumberOfSelfLoops()
+        {
+            int count = 0;
+            for (int v = 0; v < _V; v++)
+            {
+                if (adjMatrix[v, v])
+                    count++;
+            }
+            return count;
+        }
+
+        public override string ToString()
+        {
+            var sb = new System.Text.StringBuilder();
+            sb.AppendLine($"{_V} vertices, {_E} edges");
+            for (int v = 0; v < _V; v++)
+            {
+                sb.Append(v + ": ");
+                foreach (int w in Adj(v))
+                {
+                    sb.Append(w + " ");
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ INCLASSGRAPHSAHHHPAIN/INCLASSGRAPHSAHHHPAIN/Program.cs
@@ -0,0 +1,36 @@
+﻿using INCLASSGRAPHSAHHHPAIN;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating a graph with 5 vertices
+        Graph g = new Graph(5);
+
+        // Add some edges
+        g.AddEdge(0, 1);
+        g.AddEdge(0, 2);
+        g.AddEdge(3, 4);
+        g.AddEdge(1, 2);
+
+        // Print graph structure
+        Console.WriteLine(g.ToString());
+
+        // Get the dets
+        Console.WriteLine("Number of Vertices: " + g.V());
+        Console.WriteLine("Number of Edges: " + g.E());
+        Console.WriteLine("Max Degree: " + g.MaxDegree());
+        Console.WriteLine("Average Degree: " + g.AverageDegree());
+        Console.WriteLine("Number of Self Loops: " + g.NumberOfSelfLoops());
+
+        
+        Console.Write("Adjacency for vertex 0: ");
+        foreach (int v in g.Adj(0))
+        {
+            Console.Write(v + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
